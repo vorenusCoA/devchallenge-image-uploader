1bc6460fdc78f0be522b5c12177034559895f9cf
Rodrigo Oscar Gonzalez <ro_gonzalez@hotmail.com>
2023-06-20 18:48:51 +0200

    initial commit

---
+++ backend/src/main/java/com/example/devchallengeimageuploaderapi/DevchallengeImageUploaderApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.devchallengeimageuploaderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevchallengeImageUploaderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevchallengeImageUploaderApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/devchallengeimageuploaderapi/DevchallengeImageUploaderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devchallengeimageuploaderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevchallengeImageUploaderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
